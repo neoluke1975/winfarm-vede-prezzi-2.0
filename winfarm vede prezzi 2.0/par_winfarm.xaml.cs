58d8d38e64e30ed697d266d58e86d1cd45c934ee
gtroncone <gtroncone@PC-TRONCONE>
2016-10-29 15:01:17 +0200

    29/10/2016

--- winfarm vede prezzi 2.0/par_winfarm.xaml.cs
+++ winfarm vede prezzi 2.0/par_winfarm.xaml.cs
@@ -16,63 +16,26 @@ namespace winfarm_vede_prezzi_2._0
         public par_winfarm()
         {
             InitializeComponent();
-            XmlDocument doc = new XmlDocument();
-            using (Stream lettore = File.OpenRead("c:/kiosk/parametri.xml"))
-            {
-                doc.Load(lettore);
-                XmlElement percorso = (XmlElement)doc.DocumentElement.LastChild;
-                XmlElement percorso_server = doc.DocumentElement.ChildNodes[1]["percorso_server", "parametri winfarm"];
-                XmlElement percorso_winfarm = doc.DocumentElement.ChildNodes[2]["percorso_winfarm", "parametri winfarm"];
-                XmlElement percorsoIntestazione = doc.DocumentElement.ChildNodes[3]["percorso_intestazione", "parametri winfarm"];
-                XmlElement percorsoTempo = doc.DocumentElement.ChildNodes[4]["percorso_tempo", "parametri winfarm"];
-                string server = percorso_server.FirstChild.Value;
-                string stringa_winfarm = percorso_winfarm.FirstChild.Value;
-                string intestazione = percorsoIntestazione.FirstChild.Value;
-                string tempo = percorsoTempo.FirstChild.Value;
-                tbx_server.Text = server.ToString();
-                tbx_percorso.Text = stringa_winfarm.ToString();
-                tbxIntestazione.Text = intestazione.ToString();
-                tbxTime.Text = tempo.ToString();
-            }
+
+            tbx_server.Text = winfarm_vede_prezzi_2._0.Properties.Settings.Default.server;
+            tbx_percorso.Text = winfarm_vede_prezzi_2._0.Properties.Settings.Default.percorso;
+            tbxIntestazione.Text = winfarm_vede_prezzi_2._0.Properties.Settings.Default.farmacia;
+            tbxTime.Text = (winfarm_vede_prezzi_2._0.Properties.Settings.Default.timer).ToString();
+
 
         }
 
 
         private void btn_salva_Click(object sender, RoutedEventArgs e)
         {
-            XmlWriterSettings settings = new XmlWriterSettings();
-            settings.Indent = true;
-            settings.NewLineOnAttributes = true;
+            winfarm_vede_prezzi_2._0.Properties.Settings.Default.server = tbx_server.Text;
+           winfarm_vede_prezzi_2._0.Properties.Settings.Default.percorso = tbx_percorso.Text;
+            winfarm_vede_prezzi_2._0.Properties.Settings.Default.farmacia= tbxIntestazione.Text;
+            winfarm_vede_prezzi_2._0.Properties.Settings.Default.timer=int.Parse(tbxTime.Text);
+            Properties.Settings.Default.Save();
+            this.Close();
 
-            string ns = "parametri winfarm";
 
-            using (XmlWriter writer = XmlWriter.Create("c:/kiosk/parametri.xml", settings))
-            {
-                writer.WriteStartDocument();
-
-                writer.WriteStartElement("parametri_winfarm_vedeprezzi", ns);
-                writer.WriteComment("winfam_parametri");
-
-                writer.WriteStartElement("parametri_server", ns);
-                writer.WriteAttributeString("server", "1");
-                writer.WriteElementString("percorso_server", ns, tbx_server.Text);
-                writer.WriteEndElement();
-                writer.WriteStartElement("parametri_server", ns);
-                writer.WriteAttributeString("server", "2");
-                writer.WriteElementString("percorso_winfarm", ns, tbx_percorso.Text);
-                writer.WriteEndElement();
-                writer.WriteStartElement("parametri_server", ns);
-                writer.WriteAttributeString("server", "3");
-                writer.WriteElementString("percorso_intestazione", ns, tbxIntestazione.Text);
-                writer.WriteEndElement();
-                writer.WriteStartElement("parametri_server", ns);
-                writer.WriteAttributeString("server", "4");
-                writer.WriteElementString("percorso_tempo", ns, tbxTime.Text);
-                writer.WriteEndElement();
-                writer.WriteEndDocument();
-                this.Close();
-
-            }
         }
 
         private void comboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
